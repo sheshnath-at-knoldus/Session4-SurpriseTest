1db3d1878cc798a145d3802167ed2916dc3b5c82
sheshu2906 <sheshnath.yadav@knoldus.com>
2023-03-28 15:26:46 +0530

    Surprise Test

--- Session4Surprise/src/main/scala/SurpriseTest.scala
+++ Session4Surprise/src/main/scala/SurpriseTest.scala
@@ -6,21 +6,23 @@ case class Num(number:String ,isEven :Boolean)
 object SurpriseTest extends App{
 
 
-  val listOfNum = List(Num("1", false), Num("2", true), Num("3", false), Num("4", true))
+  val listOfNum = List(
+    Num("1", false),
+    Num("2", true),
+    Num("3", false),
+    Num("4", true)
+  )
 
   val filterEvenNumber = listOfNum.filter(value => value.number.toInt % 2 == 0)
   val filterOddNumber = listOfNum.filter(value => !value.isEven)
 
   val partitionList = listOfNum.partition(value => value.isEven)
-  val firstElement = listOfNum.map(value=>value.number)
-
+  val firstElement = listOfNum.filter(value => (value.number.toInt%2==0)).map(value => value.number )
 
   println("Filtered EvenNumber: "+filterEvenNumber)
   println("Filtered OddNumber : "+filterOddNumber)
   println("Partition : "+partitionList)
   println("First element of List :"+firstElement)
-
-
 
 }
 
